3e4d5282f960b2727fb57848fd97d2d8c5c02a06
Данияр Иркагалиев <Daniyar_Irkagaliev@mail.ru>
2024-11-14 02:04:51 +0300

    added the use of the App component and the transfer of props to it

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {App} from './components/app/App.tsx';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+const placesInfo = [
+  {
+    isPremium: true,
+    href: '#',
+    image: 'img/apartment-01.jpg',
+    price: 120,
+    rating: 4,
+    name: 'Beautiful & luxurious apartment at great location',
+    type: 'Apartment',
+  },
+  {
+    href: '#',
+    isBookmarked: true,
+    image: 'img/room.jpg',
+    price: 80,
+    rating: 4,
+    name: 'Wood and stone place',
+    type: 'Room',
+  },
+  {
+    href: '#',
+    image: 'img/apartment-02.jpg',
+    price: 132,
+    rating: 4,
+    name: 'Canal View Prinsengracht',
+    type: 'Apartment',
+  },
+  {
+    isPremium: true,
+    href: '#',
+    image: 'img/apartment-03.jpg',
+    price: 180,
+    rating: 4,
+    name: 'Nice, cozy, warm big bed apartment',
+    type: 'Apartment',
+  },
+  {
+    href: '#',
+    image: 'img/room.jpg',
+    price: 80,
+    isBookmarked: true,
+    rating: 4,
+    name: 'Wood and stone place',
+    type: 'Room',
+  },
+];
+
+
 root.render(
   <React.StrictMode>
-    <h1>Hello, World!</h1>
+    <App placesCount = {321} placesInfo={placesInfo}/>
   </React.StrictMode>
 );
