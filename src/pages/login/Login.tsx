a632ca5a5e62e6a6428407a041944f8ef31a7615
Данияр Иркагалиев <Daniyar_Irkagaliev@mail.ru>
2025-01-04 17:15:30 +0300

    added link to main in header

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+
 export const Login: React.FC = () => (
   <div className="page page--gray page--login">
     <header className="header">
       <div className="container">
         <div className="header__wrapper">
           <div className="header__left">
-            <a className="header__logo-link" href="main.html">
+            <Link className="header__logo-link" to="/">
               <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
-            </a>
+            </Link>
           </div>
         </div>
       </div>
