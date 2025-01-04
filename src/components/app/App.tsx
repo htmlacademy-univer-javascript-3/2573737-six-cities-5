f92d3d43135f4d71ac8c8b893513cb3941043760
Данияр Иркагалиев <Daniyar_Irkagaliev@mail.ru>
2025-01-04 17:07:13 +0300

    renamed props 'placesInfo' to 'offers' due to a change in TypePlacesInfo. added the transfer of props to favorites

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -3,27 +3,26 @@ import {Login} from '../../pages/login/Login.tsx';
 import {Offer} from '../../pages/offer/Offer.tsx';
 import {NotFound} from '../../pages/notFound/NotFound.tsx';
 import {Favorites} from '../../pages/favorites/Favorites.tsx';
-import {PlacesInfo} from '../card/Card.tsx';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import PrivateRoute from '../../pages/privateRoute/PrivateRoute.tsx';
 
-import { AuthorizationStatus} from '../../types/types.ts';
+import {AuthorizationStatus, TypePlacesInfo} from '../../types/types.ts';
 
 interface Props {
   placesCount: number;
-  placesInfo: PlacesInfo[];
+  offers: TypePlacesInfo[];
 }
 
 // временный костыль. EELint ругается: placesCount и placesInfo is missing in props validation, хотя вот же выше они объявлены
 // eslint-disable-next-line react/prop-types
-export const App: React.FC<Props> = ({placesCount, placesInfo}) => (
+export const App: React.FC<Props> = ({placesCount, offers}) => (
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Main placesCount={placesCount} placesInfo={placesInfo}/>}/>
+      <Route path="/" element={<Main placesCount={placesCount} offers={offers}/>}/>
       <Route path="/login" element={<Login/>}/>
       <Route path="/favorites" element={
-        <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
-          <Favorites/>
+        <PrivateRoute authorizationStatus={AuthorizationStatus.Auth}>
+          <Favorites offers={offers}/>
         </PrivateRoute>
       }
       />
