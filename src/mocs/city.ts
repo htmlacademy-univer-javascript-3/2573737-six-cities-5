8ad1e43b78b825ee4773c445ed7c07ab83fad152
Данияр Иркагалиев <Daniyar_Irkagaliev@mail.ru>
2025-01-04 22:14:43 +0300

    added city data

---
+++ src/mocs/city.ts
@@ -0,0 +1,34 @@
+import {TypeCity} from '../types/types.ts';
+
+export const city: TypeCity[] = [
+  {
+    title: 'Amsterdam',
+    lng: 4.889,
+    lat: 52.374
+  },
+  {
+    title: 'Dusseldorf',
+    lng: 1,
+    lat: 52
+  },
+  {
+    title: 'Cologne',
+    lng: 2,
+    lat: 52
+  },
+  {
+    title: 'Brussels',
+    lng: 3,
+    lat: 52
+  },
+  {
+    title: 'Amsterdam',
+    lng: 5,
+    lat: 52
+  },
+  {
+    title: 'Hamburg',
+    lng: 6,
+    lat: 52
+  },
+];
