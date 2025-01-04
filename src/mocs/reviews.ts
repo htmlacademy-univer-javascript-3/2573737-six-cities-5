4af4907c75cef241ada04084be3cf92fa0777d70
Данияр Иркагалиев <Daniyar_Irkagaliev@mail.ru>
2025-01-04 17:27:44 +0300

    added review mocs

---
+++ src/mocs/reviews.ts
@@ -0,0 +1,24 @@
+import {TypeReviewInfo} from '../types/types.ts';
+
+export const reviews: TypeReviewInfo[] = [
+  {
+    id: 1,
+    userId: 431,
+    placeId: 2,
+    comment: 'Все понравилось, кроме цены',
+    rating: 3,
+    reviewTime: '2019-04-24'
+  },
+  {
+    id: 2,
+    userId: 41,
+    placeId: 1,
+    comment: 'Красивое',
+    rating: 4,
+    reviewTime: '2019-04-24'
+  },
+];
+// user id => user_img, user_name
+// rating
+// review_text
+// review_time
