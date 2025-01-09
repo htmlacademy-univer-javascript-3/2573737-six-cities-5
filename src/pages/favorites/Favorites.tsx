9e93202dae726be0920399fcd4839ae2ab1ee9e5
Данияр Иркагалиев <Daniyar_Irkagaliev@mail.ru>
2025-01-10 00:08:27 +0300

    header is now a component

--- src/pages/favorites/Favorites.tsx
+++ src/pages/favorites/Favorites.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import {FavoriteCardList} from '../../components/cardList/FavoriteCardList.tsx';
 import {TypePlacesInfo} from '../../types/types.ts';
+import {Header} from '../../components/header/Header.tsx';
 
 interface FavPlaces {
   offers: TypePlacesInfo[];
@@ -9,34 +10,7 @@ interface FavPlaces {
 
 export const Favorites: React.FC<FavPlaces> = ({offers}) => (
   <div className="page">
-    <header className="header">
-      <div className="container">
-        <div className="header__wrapper">
-          <div className="header__left">
-            <Link className="header__logo-link" to="/">
-              <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
-            </Link>
-          </div>
-          <nav className="header__nav">
-            <ul className="header__nav-list">
-              <li className="header__nav-item user">
-                <a className="header__nav-link header__nav-link--profile" href="#">
-                  <div className="header__avatar-wrapper user__avatar-wrapper">
-                  </div>
-                  <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                  <span className="header__favorite-count">3</span>
-                </a>
-              </li>
-              <li className="header__nav-item">
-                <a className="header__nav-link" href="#">
-                  <span className="header__signout">Sign out</span>
-                </a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </div>
-    </header>
+    <Header isMainPage={false}/>
 
     <main className="page__main page__main--favorites">
       <div className="page__favorites-container container">
