65f0f6e4a6f19c0b17a139f4f3ea687026c7b985
Данияр Иркагалиев <Daniyar_Irkagaliev@mail.ru>
2025-01-10 00:08:03 +0300

    added Redux action utils

---
+++ src/hooks/hooks.ts
@@ -0,0 +1,5 @@
+import { State, AppDispatch} from '../types/types.ts';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<State> = useSelector;
