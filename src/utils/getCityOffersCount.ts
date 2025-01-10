cad908e9bb2ba0c86d427424b9e8d296f9fade0f
Данияр Иркагалиев <Daniyar_Irkagaliev@mail.ru>
2025-01-10 21:56:07 +0300

    deleted

--- src/utils/getCityOffersCount.ts
+++
@@ -1,8 +0,0 @@
-import {City} from '../types/types.ts';
-import getCityOffers from './getCityOffers.ts';
-
-const getCityOffersCount = (city: City): number => (
-  getCityOffers(city).length
-);
-
-export default getCityOffersCount;
