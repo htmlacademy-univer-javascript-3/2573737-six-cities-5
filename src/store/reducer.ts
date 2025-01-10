49222eccf117772fba557984ecf2dc7bdd422862
Данияр Иркагалиев <Daniyar_Irkagaliev@mail.ru>
2025-01-10 22:04:37 +0300

    added default data and some actions

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,20 +1,34 @@
 import {city} from '../mocs/city.ts';
 import {TypePlacesInfo, TypeCity} from '../types/types.ts';
 import { createReducer } from '@reduxjs/toolkit';
-import { setCity, setOffers } from './action.ts';
-import getCityOffers from '../utils/getCityOffers.ts';
+import {setCity, setError, setOffers, setOffersByCity, setOffersLoading} from './action.ts';
 
 
 type InitialState = {
   currentCity: TypeCity;
   cities: TypeCity[];
   offers: TypePlacesInfo[];
+  offersByCity: TypePlacesInfo[];
+  offersLoading: boolean;
+  error: string | null;
+};
+
+const defaultCity: TypeCity = {
+  name: 'Paris',
+  location: {
+    latitude: 48.85661,
+    longitude: 2.351499,
+    zoom: 13
+  }
 };
 
 const initialState: InitialState = {
-  currentCity: city[0],
+  currentCity: defaultCity,
   cities: city,
-  offers: getCityOffers('Paris'),
+  offers: [],
+  offersByCity: [],
+  offersLoading: false,
+  error: null,
 };
 
 export const reducer = createReducer(initialState, (builder) => {
@@ -24,5 +38,14 @@ export const reducer = createReducer(initialState, (builder) => {
     })
     .addCase(setOffers, (state, action) => {
       state.offers = action.payload;
+    })
+    .addCase(setOffersByCity, (state, action) => {
+      state.offersByCity = action.payload;
+    })
+    .addCase(setOffersLoading, (state, action) => {
+      state.offersLoading = action.payload;
+    })
+    .addCase(setError, (state, action) => {
+      state.error = action.payload;
     });
 });
