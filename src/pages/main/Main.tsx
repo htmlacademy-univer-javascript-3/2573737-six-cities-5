6c18f81253837ad34ee7b2bf8eb59eb6fe9208d1
Данияр Иркагалиев <Daniyar_Irkagaliev@mail.ru>
2025-01-12 19:49:59 +0300

    added sorting function

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -16,9 +16,12 @@ export const Main: React.FC = () => {
   const offers = useAppSelector((state) => state.offers);
   const dispatch = useAppDispatch();
   const loading = useAppSelector((state) => state.offersLoading);
-  const cityOffers = offers.filter((offer: TypePlacesInfo) => offer?.city?.name === currentCity.name);
+  const [sort, setSort] = useState<TypeSortFilters>(TypeSortFilters.POPULAR);
+  const cityOffers = useFilter({offers, currentCity, sort});
   const [offerCount, setOfferCount] = useState<number>();
-
+  const handleSortChange = (newSortType: TypeSortFilters) => {
+    setSort(newSortType);
+  };
   useEffect(() => {
     if (!loading) {
       dispatch(setOffersByCity(cityOffers));
@@ -41,21 +44,7 @@ export const Main: React.FC = () => {
               <section className="cities__places places">
                 <h2 className="visually-hidden">Places</h2>
                 <b className="places__found">{offerCount} places to stay in {currentCity.name}</b>
-                <form className="places__sorting" action="#" method="get">
-                  <span className="places__sorting-caption">Sort by</span>
-                  <span className="places__sorting-type" tabIndex={0}>
-                Popular
-                    <svg className="places__sorting-arrow" width="7" height="4">
-                      <use xlinkHref="#icon-arrow-select"/>
-                    </svg>
-                  </span>
-                  <ul className="places__options places__options--custom places__options--opened">
-                    <li className="places__option places__option--active" tabIndex={0}>Popular</li>
-                    <li className="places__option" tabIndex={0}>Price: low to high</li>
-                    <li className="places__option" tabIndex={0}>Price: high to low</li>
-                    <li className="places__option" tabIndex={0}>Top rated first</li>
-                  </ul>
-                </form>
+                <PlacesSorting initialSort={sort} onChange={handleSortChange}/>
                 <div className="cities__places-list places__list tabs__content">
                   <CardList offers={cityOffers}/>
                 </div>
